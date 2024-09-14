812be84b1ec1d0c72d3723964b89cd01c768092a
nhakobyan8 <hakobyannarek535@gmail.com>
2024-09-14 19:49:04 +0400

    second commit5

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,20 @@
-import Image from 'next/image';
 import Head from 'next/head';
 
 export default function About() {
+  const testimonials = [
+    {
+      testimonial: '"GearRental provided us with the best equipment for our recording session. Highly recommended!"',
+      name: '- Alex',
+    },
+    {
+      testimonial: '"The support team was very helpful and the equipment was top-notch. Will rent again."',
+      name: '- Jamie',
+    },
+    {
+      testimonial: '"Excellent service and great prices. GearRental made our project a success."',
+      name: '- Taylor',
+    },
+  ];
   return (
     <>
       <Head>
@@ -19,15 +32,9 @@ export default function About() {
           <p className="text-center text-lg text-text-muted mt-4 max-w-2xl mx-auto">
             GearRental is your go-to destination for sound equipment rental. Our mission is to provide high-quality gear with excellent customer service.
           </p>
-          <div style={{ backgroundImage: `url("/images/about-us.webp")` }} className="mt-12 bg-center md:h-[600px] bg-cover flex justify-center items-center text-center">
-            <div className="flex justify-between items-center bg-black h-full bg-opacity-60 w-full flex-wrap">
-              <div className="space-y-6 flex flex-col p-10 md:w-1/2 w-full justify-center items-center">
-                <h2 className="text-3xl font-bold text-white">Our Mission</h2>
-                <p className="text-lg text-text-muted">
-                  We strive to empower creators and professionals by offering top-notch sound equipment at competitive prices. Our commitment is to ensure that every project, big or small, has access to the tools necessary for success.
-                </p>
-              </div>
-              <div className="space-y-6 flex p-10 flex-col w-full md:w-1/2 justify-center items-center">
+          <div style={{ backgroundImage: `url("/images/about-us.webp")` }} className="mt-12 rounded-xl bg-center md:h-[600px] bg-cover flex justify-center items-center text-center">
+            <div className="flex justify-between rounded-xl items-center bg-black h-full bg-opacity-60 border border-slate-400/20 w-full flex-wrap">
+              <div className="space-y-6 flex p-14 flex-col w-full md:w-1/2 justify-center items-center">
                 <h2 className="text-3xl font-bold text-white">Why Choose Us</h2>
                 <ul className="list-disc list-inside text-lg text-text-muted space-y-2">
                   <li>Wide range of high-quality equipment</li>
@@ -37,8 +44,50 @@ export default function About() {
                   <li>Flexible rental terms</li>
                 </ul>
               </div>
+              <div className="space-y-6 flex flex-col p-10 md:w-1/2 w-full justify-center items-center">
+                <h2 className="text-3xl font-bold text-white">Our Mission</h2>
+                <p className="text-lg text-text-muted">
+                  We strive to empower creators and professionals by offering top-notch sound equipment at competitive prices. Our commitment is to ensure that every project, big or small, has access to the tools necessary for success.
+                </p>
+              </div>
+
             </div>
           </div>
+
+          <section className="mt-20">
+            <h2 className="text-3xl text-center md:text-4xl font-bold text-text">What Our Clients Say</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10 text-text-muted">
+              {testimonials.map((testimonial, index) => (
+                <div
+                  key={index}
+                  className="space-y-4 bg-background-light p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                >
+                  <p>{testimonial.testimonial}</p>
+                  <p className="text-lg font-semibold">{testimonial.name}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+          <section className="mt-20">
+            <h2 className="text-3xl md:text-4xl font-bold text-text">Stay Updated</h2>
+            <p className="text-lg text-text-muted mt-4">Subscribe to our newsletter to get the latest news and special offers.</p>
+            <form className="mt-8 flex flex-col md:flex-row items-center w-full">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full md:flex-grow px-4 py-2 rounded-md bg-background text-text-muted focus:outline-none focus:ring-2 focus:ring-primary-light"
+                aria-label="Enter your email"
+                required
+              />
+              <button
+                type="submit"
+                className="mt-4 md:mt-0 md:ml-4 px-6 py-3 w-full md:w-auto bg-primary text-white rounded-md hover:bg-primary-dark transition-transform duration-300 ease-in-out transform hover:scale-105 shadow-lg focus:outline-none focus:ring-2 focus:ring-primary-light"
+                aria-label="Subscribe to newsletter"
+              >
+                Subscribe
+              </button>
+            </form>
+          </section>
         </div>
       </div>
     </>
--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -74,77 +74,87 @@ const SinglePage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-background-light text-text px-3 py-12">
-      <div className="max-w-4xl mx-auto bg-background p-8 rounded-lg shadow-lg">
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="md:w-1/2 mb-6 md:mb-0">
+    <div className="min-h-screen bg-background-light text-text px-4 py-12">
+      <div className="max-w-6xl mx-auto bg-background p-10 rounded-lg shadow-2xl">
+        <div className="flex flex-col items-center md:flex-row mb-10">
+          <div className="md:w-1/2 mb-8 md:mb-0 cursor-pointer group owerflow-hidden">
             <Image
               src={product.imageUrl}
               alt={product.name}
               objectFit="cover"
-              width={500}
-              height={500}
-              className="w-full h-auto rounded-lg shadow-md"
+              width={600}
+              height={600}
+              className="w-full h-auto rounded-lg transition-transform duration-300 ease-in-out transform group-hover:scale-105"
               aria-label={product.name}
             />
           </div>
-          <div className="md:w-1/2 md:pl-8">
+          <div className="md:w-1/2 md:pl-10">
             <h1 className="text-4xl font-bold mb-4">{product.name}</h1>
             <p className="text-lg text-text-muted mb-4">{product.description}</p>
-            <p className="text-primary font-bold text-2xl mb-6">${product.price} per day</p>
-
-            {product.availableQuantity !== 0 && <h2 className="text-xl font-semibold mb-2">Available Quantity: {product.availableQuantity}</h2>}
-            {product.availableQuantity === 0 && (
-              <p className="text-red-500 mb-6">This product is currently out of stock for rental.</p>
+            <p className="text-primary font-bold text-3xl mb-6">${product.price} per day</p>
+  
+            {product.availableQuantity !== 0 ? (
+              <h2 className="text-xl font-semibold mb-4">In Stock: {product.availableQuantity}</h2>
+            ) : (
+              <p className="text-red-500 text-lg mb-6">Currently out of stock for rental.</p>
             )}
-
+          </div>
+        </div>
+        <div className="grid grid-cols-1 items-center md:grid-cols-2 gap-10">
+          <div>
             <h2 className="text-2xl font-semibold mb-4">Features</h2>
-            <ul className="list-disc list-inside mb-8">
+            <ul className="list-disc list-inside space-y-3">
               {product.features?.map((feature, index) => (
                 <li key={index}>{feature}</li>
               ))}
             </ul>
-
-            <h2 className="text-2xl font-semibold mb-4">Specifications</h2>
-            <ul className="list-inside mb-8">
+  
+            <h2 className="text-2xl font-semibold mt-8 mb-4">Specifications</h2>
+            <ul className="space-y-2">
               {product.specifications && Object.entries(product.specifications).map(([key, value]) => (
                 <li key={key} className="mb-2">
                   <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {value}
                 </li>
               ))}
             </ul>
-
+          </div>
+          <div>
             <h2 className="text-2xl font-semibold mb-4">Customer Reviews</h2>
             <div className="space-y-4">
               {product.reviews?.map((review, index) => (
                 <div key={index} className="bg-background-light p-4 rounded-lg shadow-md">
-                  <p className="text-lg mb-2"><strong>{review.user}</strong> rated it {review.rating} stars</p>
+                  <p className="text-lg mb-2">
+                    <strong>{review.user}</strong> rated it {review.rating} stars
+                  </p>
                   <p>{review.comment}</p>
                 </div>
               ))}
             </div>
-
-            <div className="mt-8 flex space-x-4">
-              <button
-                onClick={addToCartHandler}
-                className={`px-6 py-3 bg-primary text-white rounded-md hover:bg-primary-dark transition-transform duration-300 ease-in-out transform hover:scale-105 shadow-lg focus:outline-none focus:ring-2 focus:ring-primary-light ${product.availableQuantity === 0 ? 'opacity-50 cursor-not-allowed' : ''
-                  }`}
-                disabled={product.availableQuantity === 0}
-              >
-                Add to Cart
-              </button>
-              <button
-                onClick={goBack}
-                className="px-6 py-3 bg-secondary text-white rounded-md hover:bg-secondary-dark transition-transform duration-300 ease-in-out transform hover:scale-105 shadow-lg focus:outline-none focus:ring-2 focus:ring-secondary-light"
-              >
-                Go Back
-              </button>
-            </div>
           </div>
+        </div>
+  
+        {/* Секция с кнопками действий */}
+        <div className="mt-10 flex justify-start space-x-4">
+          <button
+            onClick={addToCartHandler}
+            className={`px-6 py-3 bg-primary text-white rounded-lg hover:bg-primary-dark transition duration-300 ease-in-out transform shadow-lg focus:outline-none focus:ring-2 focus:ring-primary-light ${
+              product.availableQuantity === 0 ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            disabled={product.availableQuantity === 0}
+          >
+            Add to Cart
+          </button>
+          <button
+            onClick={goBack}
+            className="px-6 py-3 border border-slate-400 bg-secondary text-white rounded-lg hover:bg-secondary-dark transition duration-300 ease-in-out transform hover:scale-105 shadow-lg focus:outline-none focus:ring-2 focus:ring-secondary-light"
+          >
+            Go Back
+          </button>
         </div>
       </div>
     </div>
   );
+  
 };
 
 export default SinglePage;
